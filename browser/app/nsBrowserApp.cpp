a710c15f97f95894c813123adb50dfe204680472
aubymori <aubyomori@gmail.com>
2024-12-17 19:48:48 -0600

    [browser] Disable launcher process on Windows The pre-XUL skeleton UI did not exist in old Firefox and does not fit its UI design language.

--- browser/app/nsBrowserApp.cpp
+++ browser/app/nsBrowserApp.cpp
@@ -29,9 +29,11 @@
 #include "nsCOMPtr.h"
 
 #ifdef XP_WIN
-#  include "mozilla/PreXULSkeletonUI.h"
-#  include "freestanding/SharedSection.h"
-#  include "LauncherProcessWin.h"
+#  ifdef MOZ_LAUNCHER_PROCESS
+#    include "mozilla/PreXULSkeletonUI.h"
+#    include "freestanding/SharedSection.h"
+#    include "LauncherProcessWin.h"
+#  endif
 #  include "mozilla/GeckoArgs.h"
 #  include "mozilla/mscom/ProcessRuntime.h"
 #  include "mozilla/WindowsDllBlocklist.h"
@@ -162,7 +164,7 @@ static bool IsArg(const char* arg, const char* s) {
 Bootstrap::UniquePtr gBootstrap;
 
 static int do_main(int argc, char* argv[], char* envp[]) {
-  // Allow firefox.exe to launch XULRunner apps via -app <application.ini>
+  // Allow nara.exe to launch XULRunner apps via -app <application.ini>
   // Note that -app must be the *first* argument.
   const char* appDataFile = getenv("XUL_APP_FILE");
   if ((!appDataFile || !*appDataFile) && (argc > 1 && IsArg(argv[1], "app"))) {
@@ -442,13 +444,14 @@ int main(int argc, char* argv[], char* envp[]) {
     auto result = mozilla::WindowsDpiInitialization();
     (void)result;  // Ignore errors since some tools block DPI calls
   }
+  #if defined(MOZ_LAUNCHER_PROCESS)
+    // Once the browser process hits the main function, we no longer need
+    // a writable section handle because all dependent modules have been
+    // loaded.
+    mozilla::freestanding::gSharedSection.ConvertToReadOnly();
 
-  // Once the browser process hits the main function, we no longer need
-  // a writable section handle because all dependent modules have been
-  // loaded.
-  mozilla::freestanding::gSharedSection.ConvertToReadOnly();
-
-  mozilla::CreateAndStorePreXULSkeletonUI(GetModuleHandle(nullptr), argc, argv);
+    mozilla::CreateAndStorePreXULSkeletonUI(GetModuleHandle(nullptr), argc, argv);
+  #endif
 #endif
 
   nsresult rv = InitXPCOMGlue(LibLoadingStrategy::ReadAhead);
@@ -464,7 +467,7 @@ int main(int argc, char* argv[], char* envp[]) {
 
   int result = do_main(argc, argv, envp);
 
-#if defined(XP_WIN)
+#if defined(XP_WIN) && defined(MOZ_LAUNCHER_PROCESS)
   CleanupProcessRuntime();
 #endif
 
